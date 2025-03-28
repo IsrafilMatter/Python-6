8ab6f55b29522593d54a0498c1562cb522a5096e
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 21:48:20 +0800

    pseudocode

---
+++ center_align_string.py
@@ -0,0 +1,10 @@
+# Program to center align string with spaces without using center()
+# Author: Israfil Palabay
+# Date: March 28, 2025
+
+# Find how many extra spaces are needed
+# If the text is shorter than the width
+# Divide spaces: half on the left, half on the right
+# Add spaces around the text
+# If text is already wide enough, return it as is
+# Print Results
