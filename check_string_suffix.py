d0de022ffd16c2a84e1bd7d4ad448081d494d0eb
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 22:18:18 +0800

    Defining the string ending without using endswith() function

--- check_string_suffix.py
+++ check_string_suffix.py
@@ -2,6 +2,12 @@
 # Author: Israfil Palabay
 # Date: March 28, 2025
 
-# Check if `suffix` is longer than `s`, in which case `s` cannot end with `suffix`
- # Compare the last `len(suffix)` characters of `s` with `suffix`
- # Print results
+def ends_with(s, suffix):
+    # Check if `suffix` is longer than `s`, in which case `s` cannot end with `suffix`
+    if len(suffix) > len(s):
+        return False
+    
+    # Compare the last `len(suffix)` characters of `s` with `suffix`
+    return s[-len(suffix):] == suffix  
+
+print(ends_with("Hello World", "World"))  # Print Results
