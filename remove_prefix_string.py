47461bc70a11a4909445efdcfb676c9237f10007
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 21:35:34 +0800

    pseudocode

---
+++ remove_prefix_string.py
@@ -0,0 +1,8 @@
+# Program to remove prefix from string without using removeprefix()
+# Author: Israfil Palabay
+# Date: March 28, 2025
+
+# Check if the string `s` starts with the given `prefix`
+# If true, remove the prefix by slicing the string from index `len(prefix)` onward
+# If false, return the original string unchanged
+# Print Results
