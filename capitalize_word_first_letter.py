93cde7c7f8dd090dae969358e437a0bf2b513ab9
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 21:57:58 +0800

    pseudocode

---
+++ capitalize_word_first_letter.py
@@ -0,0 +1,9 @@
+# Program to capitalize first letter of each word without using title()
+# Author: Israfil Palabay
+# Date: March 28, 2025
+
+# Splitting the string into words, capitalize the first letter of each word,
+# Make the rest of the letters lowercase
+# Capitalize first letter, lowercase rest
+# Split string into words
+# Print Results
