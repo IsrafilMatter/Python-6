a93d6509a2f2843c9f9393223ffbc02011525c15
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 22:23:44 +0800

    center aligning the string with spaces without using center() function

--- center_align_string.py
+++ center_align_string.py
@@ -2,9 +2,20 @@
 # Author: Israfil Palabay
 # Date: March 28, 2025
 
-# Find how many extra spaces are needed
-# If the text is shorter than the width
-# Divide spaces: half on the left, half on the right
-# Add spaces around the text
-# If text is already wide enough, return it as is
-# Print Results
+def center_text(s, width):
+    # Find how many extra spaces are needed
+    pad = width - len(s)
+    
+    # If the text is shorter than the width
+    if pad > 0:
+        # Divide spaces: half on the left, half on the right
+        left_spaces = pad // 2
+        right_spaces = pad - left_spaces
+        
+        # Add spaces around the text
+        return ' ' * left_spaces + s + ' ' * right_spaces
+    else:
+        # If text is already wide enough, return it as is
+        return s
+
+print(center_text("Hello", 10)) # Print Results
