15951060d4c52b7fdc6816a86c22405580d81c6d
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 21:41:10 +0800

    pseudocode

---
+++ check_string_uppercase.py
@@ -0,0 +1,8 @@
+# Program to check if string is uppercase without using isupper()
+# Author: Israfil Palabay
+# Date: March 28, 2025
+
+# Check if there is at least one alphabetic character in `s`
+# Check if all alphabetic characters in `s` are uppercase
+ # Return True only if both conditions are met
+ # Print results
