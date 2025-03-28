d4350b13c6164c1ac12b231ba6f7432871bdd787
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 22:07:41 +0800

    defining remove_start so prefix is remove without using removeprefix() function

--- remove_prefix_string.py
+++ remove_prefix_string.py
@@ -2,7 +2,11 @@
 # Author: Israfil Palabay
 # Date: March 28, 2025
 
-# Check if the string `s` starts with the given `prefix`
-# If true, remove the prefix by slicing the string from index `len(prefix)` onward
-# If false, return the original string unchanged
-# Print Results
+def remove_start(s, prefix):
+    # Check if the string `s` starts with the given `prefix`
+    if s.startswith(prefix): 
+        return s [len(prefix):] # If true, remove the prefix by slicing the string from index `len(prefix)` onward
+    else:
+        return s # If false, return the original string unchanged
+    
+print(remove_start("Hello World", "Hello ")) # Print Results
