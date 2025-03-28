625f57fbd1017624c44ea40ae454d48bd39c4dfc
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 21:54:24 +0800

    pseudocode

---
+++ invert_character_case.py
@@ -0,0 +1,7 @@
+# Program to swap character case without using swapcase()
+# Author: Israfil Palabay
+# Date: March 28, 2025
+
+# Converting each letter to the opposite case
+# Going through each character in the string
+# Print Results
