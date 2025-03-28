59ca98cd1113511ed927e568a288c5af42163a9f
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 21:38:37 +0800

    pseudocode

---
+++ convert_string_lowercase.py
@@ -0,0 +1,7 @@
+# Program to convert string to lowercase without using lower()
+# Author: Israfil Palabay
+# Date: March 28, 2025
+
+# Convert each character in `s` to lowercase if it is an uppercase letter (A-Z)
+# Convert uppercase to lowercase
+# Iterate over each character in `s`
