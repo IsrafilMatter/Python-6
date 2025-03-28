8aee9e9e416f63b6b2c74175c70e78165e7f685a
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 21:42:34 +0800

    pseudocode

---
+++ check_string_suffix.py
@@ -0,0 +1,7 @@
+# Program to check string ending without using endswith()
+# Author: Israfil Palabay
+# Date: March 28, 2025
+
+# Check if `suffix` is longer than `s`, in which case `s` cannot end with `suffix`
+ # Compare the last `len(suffix)` characters of `s` with `suffix`
+ # Print results
