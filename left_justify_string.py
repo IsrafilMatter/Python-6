f4f0584acffe27aba20d907fd1725af7dd576536
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 21:45:13 +0800

    pseudocode

---
+++ left_justify_string.py
@@ -0,0 +1,6 @@
+# Program to left justify string with spaces without using ljust()
+# Author: Israfil Palabay
+# Date: March 28, 2025
+
+ # If `s` is shorter than `width`, pad it with spaces on the right
+ # Output: "Hello     " (5 spaces added)
