967203e323b3a8021fe5e4dbc5dd4d942c83a2ad
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 22:13:06 +0800

    defining and checking if all are in uppercase without using isupper() function

--- check_string_uppercase.py
+++ check_string_uppercase.py
@@ -2,7 +2,14 @@
 # Author: Israfil Palabay
 # Date: March 28, 2025
 
-# Check if there is at least one alphabetic character in `s`
-# Check if all alphabetic characters in `s` are uppercase
- # Return True only if both conditions are met
- # Print results
+def is_all_upper(s):
+    # Check if there is at least one alphabetic character in `s`
+    has_letter = any(c.isalpha() for c in s)
+    
+    # Check if all alphabetic characters in `s` are uppercase
+    all_upper = all(not c.isalpha() or 'A' <= c <= 'Z' for c in s)
+    
+    # Return True only if both conditions are met
+    return has_letter and all_upper
+
+print(is_all_upper("HELLO"))  # Print Results
