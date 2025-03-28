84337f6c2bbdb15a9b5289cfe868616b1d2cf3e8
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-28 21:32:05 +0800

    pseudocode

---
+++ remove_leading_spaces.py
@@ -0,0 +1,7 @@
+# Program to remove leading whitespace characters without using lstrip()
+# Author: Israfil Palabay
+# Date: March 28, 2025
+
+# Iterate while there are spaces
+# Move index forward
+# Return the substring from the first non-space character
