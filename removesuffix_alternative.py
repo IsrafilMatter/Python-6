0e1cb0b5a3b425ec340766ee369d10afc2841685
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-31 20:38:15 +0800

    defining an alternative of removesuffix() funtion

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -2,8 +2,9 @@
 # Author: Israfil Palabay
 # Date: March 31, 2025
 
-# Function to remove a specified suffix from a string
-# Check if the string ends with the given suffix
-# Remove the suffix by slicing the string
-# Return the original string if the suffix is not found
+def custom_removesuffix(text, suffix): # Function to remove a specified suffix from a string
+    if text.endswith(suffix): # Check if the string ends with the given suffix
+        return text[:-len(suffix)]# Remove the suffix by slicing the string
+    return text # Return the original string if the suffix is not found
+
 # Print Results
