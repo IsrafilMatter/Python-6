a1a4e1d26a1a604d6b26f6c6e6b13207194e806b
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-31 20:26:56 +0800

    pseudocode

---
+++ rstrip_alternative.py
@@ -0,0 +1,7 @@
+# Program description: Replicates rstrip() functionality by removing trailing spaces
+# Author: Israfil Palabay
+# Date: March 31, 2025
+
+# Repeat while the string is not empty and the last character is a space
+# Remove the last character
+# Print Results
