1e3032d93706de749b8acb3e1bb2eae5a8d93705
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-31 20:28:50 +0800

    Defining the custom_rstrip

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -2,6 +2,8 @@
 # Author: Israfil Palabay
 # Date: March 31, 2025
 
-# Repeat while the string is not empty and the last character is a space
-# Remove the last character
+def custom_rstrip(text):
+    while text and text[-1] == ' ': # Repeat while the string is not empty and the last character is a space
+        text = text[:-1] # Remove the last character
+
 # Print Results
