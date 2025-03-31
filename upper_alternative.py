dab0fa85d06aad7545d5bc1d4525085e24631ec2
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-31 21:06:57 +0800

    pseudocode

---
+++ upper_alternative.py
@@ -0,0 +1,7 @@
+# Program description: Replicates upper() functionality by converting string to uppercase
+# Author: Israfil Palabay
+# Date: March 31, 2025
+
+# Function to convert lowercase letters to uppercase
+# Convert each lowercase letter to uppercase using ASCII values
+# Print results
