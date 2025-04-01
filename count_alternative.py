c4c10cc90dbec68b6d15e6b06ca24588ec16d03b
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-04-01 21:30:45 +0800

    pseudocode

---
+++ count_alternative.py
@@ -0,0 +1,9 @@
+# Program description: Replicates count() functionality by counting occurrences of substring
+# Author: Israfil Palabay
+# Date: March 31, 2025
+
+# For each possible starting position from end to beginning
+# Check if substring matches starting from this position
+# If match found, return the position
+# If no match found, raise ValueError
+# Print results
