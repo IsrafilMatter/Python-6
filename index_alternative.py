deeac03ed68061b23c1c2a38a6e1d242ba17909f
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-04-01 21:53:17 +0800

    pseudocode

---
+++ index_alternative.py
@@ -0,0 +1,8 @@
+# Program description: Replicates index() functionality by finding first occurrence of substring
+# Author: Israfil Palabay
+# Date: April 1, 2025
+
+# For each possible starting position in text
+# Check if substring matches starting from this position
+# If match found, return the position
+# If no match found, raise ValueError
