8befc4a87080939ab0b498984267cd4fa4838669
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-04-01 20:52:26 +0800

    pseudocode

---
+++ rjust_alternative.py
@@ -0,0 +1,7 @@
+# Program description: Replicates rjust() functionality by adding spaces at the beginning
+# Author: Israfil Palabay
+# Date: March 31, 2025
+
+# Function to right-justify a string by adding leading spaces
+# Calculate the number of spaces needed and prepend them to the text
+# Print results
