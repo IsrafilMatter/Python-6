347c2f16794c5b76c5a87edae32a896e8f1cb88c
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-04-01 22:21:09 +0800

    pseudocode

---
+++ rindex_alternative.py
@@ -0,0 +1,8 @@
+# Program description: Replicates rindex() functionality by finding last occurrence of substring
+# Author: Israfil Palabay
+# Date: April 1, 2025
+
+# For each possible starting position from end to beginning
+# Check if substring matches starting from this position
+# If match found, return the position
+# If no match found, raise ValueError
